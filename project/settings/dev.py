3b89e94fb9eba1f793e1df30268059738a92a88b
Alex Tomkins <tomkins@darkzone.net>
2024-06-12 10:59:56 +0200

    Initial

---
+++ project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9@q4vn+!8mu-$s%=9qqq87^oy5cfm2p=i=-%!3ge!51)ur3h5l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
